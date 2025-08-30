e7b8fd6a1e1d7341497602826b9a92ce73640310
Shubham Kumar <shubhamkumar20040213@gmail.com>
2025-08-31 02:12:57 +0530

    fix cokkie

--- server/server.js
+++ server/server.js
@@ -51,7 +51,9 @@ app.use(session({
   cookie: {
     expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
     maxAge: 7 * 24 * 60 * 60 * 1000,
-    httpOnly: true
+    httpOnly: true,
+    secure:true,
+    sameSite: "none"
   },
 }));
 
